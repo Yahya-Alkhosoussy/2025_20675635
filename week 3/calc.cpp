bead0f99848b281b418c655993b970b732d2ebaa
Yahya Alkhosoussy <97708352+Yahya-Alkhosoussy@users.noreply.github.com>
2026-02-10 11:58:40 +0000

    Added print statements to calc.cpp

--- week 3/calc.cpp
+++ week 3/calc.cpp
@@ -4,7 +4,6 @@
 
 int main(int argc, char *argv[]){
     int a, b, c;
-    std::cout << "Ran successfully! " << std::endl;
     if (argc != 3) return 1;
     
     std::string sa = argv[1];
@@ -17,8 +16,6 @@ int main(int argc, char *argv[]){
     ssb >> b;
     
     c = add(a, b);
-    
-    std::cout << "Result is: " << c << std::endl;
     
     return 0;
 }
