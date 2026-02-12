228f3676e5623d8d5277a479f6a3b3246cc7fbc5
Yahya Alkhosoussy <97708352+Yahya-Alkhosoussy@users.noreply.github.com>
2026-02-12 16:05:13 +0000

    Added github workflows as well as returned the adder and calc files suited for doxyegn

---
+++ worksheet 5/adder.h
@@ -0,0 +1,14 @@
+#ifndef MATHSLIB_ADDER_H
+#define MATHSLIB_ADDER_H
+/** @file adder.h
+ *This file contains the declarations of all the exported functions in the maths library
+ */
+
+/**Adding function                                                          [brief description]
+* This is a function that adds two numbers                     [More detail]
+* @param a this is the first number                                [Parameter definition]
+* @param b this is the second number                          [Parameter definition]
+* @return the sum of the two numbers                          [Return value description]
+*/
+int add(int a, int b);
+#endif
---
+++ worksheet 5/calc.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <sstream>
+#include "adder.h"
+
+int main(int argc, char *argv[]){
+    int a, b, c;
+    std::cout << "Running" << std::endl;
+    if (argc != 3) return 1;
+    
+    std::string sa = argv[1];
+    std::string sb = argv[2];
+    
+    std::stringstream ssa(sa);
+    std::stringstream ssb(sb);
+    
+    ssa >> a;
+    ssb >> b;
+    
+    c = add(a, b);
+    
+    return 0;
+}
